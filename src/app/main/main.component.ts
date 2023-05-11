6c17ba5750548a226048147075670d5bf035b1c7
Julien Wallart <julien.wallart@outlook.com>
2023-05-11 14:51:28 +0200

    Autoscroll added.

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-main',
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./main.component.css']
 })
 export class MainComponent {
+  @ViewChild('container') container: any;
+  stickyPos = 0.75;
+  lastHeight = 0;
+  initialScrollDone = false;
 
+  ngAfterViewChecked() {
+    const element = this.container.nativeElement.getElementsByTagName('chat-history')[0];
+    if (this.lastHeight !== element.scrollHeight) {
+      console.log('Change detected');
+      this.lastHeight = element.scrollHeight;
+      if (this.initialScrollDone) {
+        const scrollHeight = element.scrollHeight;
+        const percentPos = element.scrollTop / scrollHeight;
+        if (percentPos >= this.stickyPos) {
+          element.scrollTop = scrollHeight;
+        }
+      } else {
+        this.initialScrollDone = true;
+        element.scrollTop = element.scrollHeight;
+      }
+    }
+  }
 }
