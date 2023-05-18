84906f3c07e5f4389b8dfdadc435cdb9ce2030a0
Julien Wallart <julien.wallart@outlook.com>
2023-05-18 21:33:29 +0200

    Added avatar status indicator

--- src/app/main/top-bar/top-bar.component.ts
+++ src/app/main/top-bar/top-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { StatusService } from '../../services/status.service';
 import { HyperionService } from '../../services/hyperion.service';
 import { LocalStorageService } from '../../services/local-storage.service';
@@ -10,11 +10,14 @@ import { LocalStorageService } from '../../services/local-storage.service';
   styleUrls: ['./top-bar.component.css']
 })
 export class TopBarComponent {
+  @ViewChild('indicator') indicator: any;
+
   models: any;
   selectedModel: string;
   prompts: any;
   selectedPrompt: string;
   state: string = '';
+  bot: string = '';
 
   constructor(public hyperion: HyperionService, private store: LocalStorageService, private status: StatusService) {
     this.selectedModel = this.store.getItem('model') !== null ? this.store.getItem('model') : '';
@@ -25,10 +28,23 @@ export class TopBarComponent {
 
     this.models = [];
     this.prompts = [];
-    this.status.state$.subscribe((state) => this.state = state);
-  }
 
-  ngOnInit() {
+    this.status.state$.subscribe((state) => {
+      this.state = state;
+      const el = this.indicator.nativeElement;
+      if (this.state === 'offline') {
+        el.setAttribute('class', 'dot');
+      } else if (this.state === 'online') {
+        el.setAttribute('class', 'dot green');
+      } else if (this.state === 'sleeping') {
+        el.setAttribute('class', 'dot orange');
+      } else {
+        el.setAttribute('class', 'dot red');
+      }
+    });
+
+    this.hyperion.botName$.subscribe((res) => this.bot = res);
+
     this.hyperion.models$.subscribe((res) => this.models = res);
     this.hyperion.model$.subscribe((res) => this.selectedModel = res as string);
 
